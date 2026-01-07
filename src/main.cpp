ca90b6dbe055dbf1f0514a9efefa95d0a61ed865
Grigoriy Arzhevitin <arzhevitin.g@gmail.com>
2026-01-07 14:12:01 +0500

    add support for passing path to json file via cli args

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include "includes/runtime.h"
 
+bool is_valid_path(const std::filesystem::path& path) {
+    if (!std::filesystem::exists(path)) {
+        std::printf("%s does not exist, use ./data.json instead\n", path.c_str());
+        return false;
+    }
+
+    if (std::filesystem::is_directory(path)) {
+        std::printf("%s is directory, use ./data.json instead\n", path.c_str());
+        return false;
+    }
+
+    if (path.extension() != ".json") {
+        std::printf("%s is not json file, use ./data.json instead\n", path.c_str());
+        return false;
+    }
+
+    return true;
+}
+
 int main(int argc, char** argv) {
-    Runtime runtime("data.json");
+    std::filesystem::path data_path = "data.json";
+
+    if (argc > 1 && is_valid_path(argv[1])) {
+        data_path = argv[1];
+    }
+
+    Runtime runtime(data_path);
     runtime.run();
 }
